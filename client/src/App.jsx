e1a351c9a398df9cc52eff2fd987b4c6d313d249
Becky <becky.gagnon92@gmail.com>
2021-02-09 18:49:25 -0800

    Front end tests framework set up, but test is not yet passing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,7 @@ import "@fontsource/roboto";
 import "@fontsource/roboto/700.css";
 import '../dist/styles.css';
 
-
-
 var TEST_MODE = true;
-
-
-
-
-
-
-
 
 class App extends React.Component {
 
---
+++ client/src/App.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { configure, shallow, mount, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App from './App.jsx';
+
+
+configure({ adapter: new Adapter() });
+
+describe('<App />', () => {
+
+  it('mounts a thing', () => {
+
+    const wrapper = mount(<App />);
+    expect(wrapper.find('#SummaryTitleDiv')).to.have.lengthOf(1);
+  })
+})
+
+
+
