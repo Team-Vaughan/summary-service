1e7dfe6ee4cd5ce1adcb9bf846943624f720ec84
amosNash <66794449+amosNash@users.noreply.github.com>
2021-04-22 12:01:43 -0600

    Setting up cache for /rooms/:id route

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
 require('newrelic');
+const redis = require('redis');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-// const db = require('../database/db.js');
 const {SERVER_PORT} = require('../config.js');
-// const couch = require('../database/couch.js');
 const controllers = require('./controllers.js');
+const { getRoomSummary} = require('../database/crudOperations.js');
 
+
+const redisPort = 6379;
+
+const client = redis.createClient(redisPort);
+
+client.on("connect", () => {
+  console.log('redis is connected');
+})
+client.on("error", (err) => {
+  console.log(err);
+});
 
 var app = express();
 
@@ -21,14 +32,45 @@ app.use(cors());
 
 
 
-// app.use('/rooms/:id', express.static(__dirname + '/../client/dist'));
+app.use('/rooms/:id', express.static(__dirname + '/../client/dist'));
 
 app.use(express.static(__dirname + '/../public'));
 
 
 app.post('/rooms/addRoomSummary', controllers.addSummaryInfoToRoom);
 
-app.get('/rooms/:id/summary', controllers.getRoomSummaryInfo);
+app.get('/rooms/:id/summary', async (req, res) => {
+  let stayId = req.params.id;
+    // client.set(1009, 1009, (res, err) => {
+    //   if (err) {
+    //     throw(err)
+    //   } else {
+    //     console.log(res);
+    //   }
+    // })
+  console.log('called', stayId);
+    client.get(stayId, (err, data) => {
+      if (err)
+      console.log('here with error', data);
+      if(data) {
+        console.log('here', data);
+        res.status(200).send(data);
+      } else {
+        console.log('made it here');
+        getRoomSummary(stayId, (summary, err) => {
+          if(summary === null) {
+            res.sendStatus(err);
+          } else {
+            client.set(stayId, JSON.stringify(summary), redis.print);
+            res.status(200).send(summary);
+          }
+        });
+        //  controllers.getRoomSummaryInfo(req, res);
+
+      }
+    });
+});
+// app.get('/rooms/:id/summary', controllers.getRoomSummaryInfo);
 
 app.put('/rooms/summary/:stayId', controllers.updateRoomSummary);
 
